533d0db62471a50d07443afb007bd70213bec73f
elifsahin89 <elifkaraal@gmail.com>
2024-04-12 14:01:49 -0500

    added step def. for us04

--- src/test/java/com/library/steps/BooksPageStepDef.java
+++ src/test/java/com/library/steps/BooksPageStepDef.java
@@ -3,11 +3,14 @@ package com.library.steps;
 import com.library.pages.BookPage;
 import com.library.utility.BrowserUtil;
 import com.library.utility.DB_Util;
+import com.library.utility.Driver;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
+import org.openqa.selenium.support.ui.Select;
 
 import java.util.List;
+import java.util.Map;
 
 import static java.lang.Thread.*;
 
@@ -16,6 +19,9 @@ public class BooksPageStepDef {
     BookPage bookPage = new BookPage();
     List<String> actualCategory;
 
+    String bookName;
+
+    //US 3.
     @When("the user navigates to {string} page")
     public void the_user_navigates_to_page(String nameOfModule) {
         bookPage.navigateModule(nameOfModule);
@@ -39,10 +45,54 @@ public class BooksPageStepDef {
 
         Assert.assertEquals(actualCategory,expectedCategory);
 
+    }
 
+    //US 4.
+    @When("the user searches for {string} book")
+    public void the_user_searches_for_book(String book) {
 
+        bookName=book;
+        bookPage.search.sendKeys(bookName);
+    }
+    @When("the user clicks edit book button")
+    public void the_user_clicks_edit_book_button() {
+
+        bookPage.editBook(bookName);
 
 
     }
+    @Then("book information must match the Database")
+    public void book_information_must_match_the_database() {
+
+        BrowserUtil.waitFor(4);
+
+        String UIbookName = bookPage.bookName.getAttribute("value");
+        String UIauthorName = bookPage.author.getAttribute("value");
+        String UI_ISBN = bookPage.isbn.getAttribute("value");
+        String UIdescription = bookPage.description.getAttribute("value");
+        String UI_year = bookPage.year.getAttribute("value");
+
+        Select categoryDropDown = new Select(bookPage.categoryDropdown);
+        String UI_category = categoryDropDown.getFirstSelectedOption().getText();
+
+        DB_Util.runQuery("select b.name, b.isbn, b.year, b.author,b.description, bc.name from books b\n" +
+                "inner join book_categories bc\n" +
+                "on b.book_category_id = bc.id\n" +
+                "where b.name='"+ bookName +"'");
+
+        Map<String, String> expectedInfoDB = DB_Util.getRowMap(1);
+
+
+
+        Assert.assertEquals(expectedInfoDB.get("b.name"), UIbookName);
+        Assert.assertEquals(expectedInfoDB.get("isbn"), UI_ISBN);
+        Assert.assertEquals(expectedInfoDB.get("year"), UI_year);
+        Assert.assertEquals(expectedInfoDB.get("author"), UIauthorName);
+        Assert.assertEquals(expectedInfoDB.get("description"), UIdescription);
+        Assert.assertEquals(expectedInfoDB.get("bc.name"), UI_category);
+
+
+    }
+
 
 }
--- src/test/resources/queries/library.sql
+++ src/test/resources/queries/library.sql
@@ -16,3 +16,8 @@ where is_returned = 0;
 
 select name from book_categories;
 
+select b.name, b.isbn, b.year, b.author,b.description, bc.name from books b
+inner join book_categories bc
+on b.book_category_id = bc.id
+where b.name='Clean Code';
+
